f3b4b9953f371b2636ab26e0c171cda09879259c
VWood <vwood.org@gmail.com>
2013-04-17 17:01:22 +1200

    notes.

--- animation.py
+++ animation.py
@@ -1,7 +1,12 @@
 
+# Use states for animation
+# States come from entity / Animation set
+# Entities have a list of animations?
+# Prehab sets of animations
+
 class Animation(object):
     def __init__(self):
-        pass
+        pass 
 
     def draw(self, surface, tick):
         pass
